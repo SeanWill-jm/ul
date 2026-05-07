4635afe8de8a7fc4eb8926e41326aa65c3c17521
Divyesh <macos@Macs-Mac-Pro.local>
2026-05-07 13:09:57 +0530

    [ADD;2C]

--- ul_sale_menu_ext/__manifest__.py
+++ ul_sale_menu_ext/__manifest__.py
@@ -1,15 +1,18 @@
 {
     'name': 'UL Sale Menu Extension',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Sales',
-    'summary': 'Add Accounting menus in Sales',
+    'summary': 'Add Accounting menus in Sales and Return Notifications',
     'description': """
         Add existing Accounting menus in Sale Order application:
         - Invoice
         - Credit Note
         - Account Receivable
+        
+        New Features:
+        - Notify Salesperson on Return Picking creation.
     """,
-    'depends': ['sale', 'account', 'account_reports'],
+    'depends': ['sale', 'account', 'account_reports', 'stock'],
     'data': [
         'security/ir.model.access.csv',
         'views/sale_menus.xml',
